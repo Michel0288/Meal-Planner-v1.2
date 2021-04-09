bcaa0f89a591b7d06d88c014faeb3f7ec4365af7
Michel Davies <micheldavies0288@gmail.com>
2021-04-09 13:56:45 -0500

    Final Submission

--- fake.py
+++ fake.py
@@ -13,7 +13,7 @@ filename="project2.sql"
 completename=os.path.join(path, filename)
 f = open(completename, "w")
 
-usertable=25
+usertable=200000
 
 for i in range(usertable):
     value=randint(0,1000)
@@ -31,7 +31,7 @@ for i in range(usertable):
 
 instruction_id=0
 ingredient_id=0
-recipetable=100
+recipetable=600000
 recipe_name=['Hard tacos','Burritos','Burgers','Meatballs and rice','Sloppy Joes','Pot Roast','BBQ Ribs','French Dip Sandwiches','Calzones','Egg Rolls','Chicken Pot Pie','Enchiladas','Stuffed Green Pepers','Curry Chicken','Baked Chicken','BBQ Pigtail','Chicken Parmesan','Chicken Nuggets','Chicken Wings','Chicken Fingers','Orange Chicken','Chessy Bacon Chicken','Chicken','Chicken Vk','Meatloaf','Stuffed Pork Chops','Beef And Broccoli','Ham','Beef or Pork Tenderloin','Salmon','Salmon Patties', 'Fish Sticks','Fish N Chips','Crab Cakes', 'Fish Tacos','Smoked Salmon Bagel','Taco Soup','Minestrone Soup','Chicken Noodle Soup','White Chicken Chili','Clam Chowder','Tomato Bisque','Italian Wedding Soup','Cheeseburger Soup','Corn Chowder','Tortellini Soup','French Onion Soup','Jambalaya','Lasagna','Royini Bake','Vegetable Lasagna','Bolognese Sauce And Pasta','Spinach-Bacon' ,'Mac N Cheese',
 'Tomato-bacon pasta','Stuffed Pasta Shells','Manicotti','Spaghetti and Meatballs','Pesto Pasta','Eggplant Parmesan','Tortellini','Stroganoff (Chicken Or Beef)','Crockpot Alfredo Lasagna','Pad Thai','Bowtie Pasta & Vegetables','BBQ Chicken Salad','Grilled Cheese Sandwiches','Tostadas','Club Salad','Grilled Chicken','Steak (Grilled Or Boiled)','Kabobs']
 instructions=['Preheat the oven to 350 degrees F.','In the bowl of an electric mixer (or using a hand mixer), beat together the brown sugar and butter until fluffy. Beat in the vanilla. Add the eggs one at a time, scraping the bowl after each one.','Mix together the flour, salt and baking soda in a medium bowl. Add it into the creamed mixture in 2 to 3 batches, mixing until just combined. Mix in the oats until just combined.','Use your preferred size cookie scoop (or a regular spoon) to drop portions of dough onto baking sheets, spacing them a couple inches apart. Bake until dark and chewy, 12 to 13 minutes. If youd like a crispier cookie, just cook a little longer!','Let the cookies cool slightly on the baking sheets, then transfer onto a plate for serving.','Add 1/2 cup finely chopped nuts to the flour mixture if youd like a nutty flavor and crunch.']
